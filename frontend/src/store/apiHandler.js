4c71096ab3924ce94c065b240f7ed73a93e304e6
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2024-12-22 20:30:15 +0530

    Update apiHandler.js

--- frontend/src/store/apiHandler.js
+++ frontend/src/store/apiHandler.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 // import { BASE_URL } from "../App.jsx";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "https://realestate-api-hu2r.onrender.com";
 
 export const apiStore = create((set, get) => ({
   authUser: null,
