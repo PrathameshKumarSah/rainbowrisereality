47c80fda2d5e07153b99b19d154cb77b5b152173
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2024-12-22 22:54:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminRoutes from "./admin-components/AdminRoutes";
 import WebsiteRoutes from "./components/WebsiteRoutes";
 
-export const ADMIN_BASE_URL = "/admin";
-export const SERVER_URL = "https://realestate-api-hu2r.onrender.com/api/";
-// const API_URL = process.env.REACT_APP_API_URL ;
+export const ADMIN_BASE_URL = import.meta.env.VITE_REACT_APP_ADMIN_BASE_URL;
+export const SERVER_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL + "/api/";
 
 export default function App() {
   // console.log(API_URL);
