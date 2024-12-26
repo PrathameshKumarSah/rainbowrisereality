88b400778554e3a434a4a37e531895f8b8618067
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2024-12-26 23:17:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,48 +2,39 @@ import express from "express"
 import dotenv from "dotenv";
 import cors from "cors";
 import webRoutes from "./src/routes/web.routes.js";
-import mysql2 from "mysql2/promise";
 import cookieParser from "cookie-parser";
 
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/imgs', express.static("imgs"));
-const corsOptions = {
-  origin: process.env.CLIENT_APP, // Allow only this origin
-  methods: ['GET', 'POST'], // Allow specific methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  credentials: true, // Enable cookies
-};
-
-app.use(cors(corsOptions));
-
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_APP,
+    credentials: true,
+  })
+  );
 
 app.use('/api', webRoutes);
 app.get('/test', (req, res) => res.send("testing"));
 
 // export default connection;
 export let connection;
+export const config = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,   // Maximum number of connections in the pool
+    queueLimit: 0          // No limit on the number of queued connection requests
+};
 
 app.listen(PORT, async () => {
     console.log(`Server is Started listening on Port ${PORT}`);
-
-    // connect to the db
-    try{
-      connection = await mysql2.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            database: process.env.DB_DATABASE,
-            password: process.env.DB_PASSWORD
-      });
-      
-      console.log("Database is Connected!");
-    } catch(err){
-      console.log("Error in connecting db");
-    }
 })
