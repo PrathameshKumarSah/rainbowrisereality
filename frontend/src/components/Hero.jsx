7b1e9281783be1d02a1a45535e8b9f7d3eb5a944
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2024-12-28 16:32:02 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Inquiry from '../components/InquiryBox'
+// import Inquiry from '../components/InquiryBox'
 import { ArrowRight } from 'lucide-react'
+import Heroback from '../assets/bgu.png'
+import Heroback1 from '../assets/bug1.png'
+import Heroback2 from '../assets/bug4.png'
+import Heroback3 from '../assets/bug5.png'
+import Heroback4 from '../assets/bug6.png'
 
 const Hero = () => {
+
+    const images = [
+        Heroback, // Replace with actual image URLs or paths
+        Heroback1,
+        Heroback2,
+        Heroback3,
+        Heroback4,
+    ];
+
+    const [currentImage, setCurrentImage] = React.useState(0);
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        }, 2000); // 3-second delay
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
   return (
    <section className='w-full'>
     {/* < Inquiry/> */}
-    <div className='bg-hero bg-center bg-cover px-4 bg-no-repeat h-[685px] w-full '>
+    <div className='bg-cover bg-center px-4 h-[685px] w-full transition-all duration-1000' style={{ backgroundImage: `url(${images[currentImage]})` }}>
         <div className='relative top-52 pl-5 sm:pl-10'>
             <span className='medium-18 text-white'>Welcome to Rainbow Rise Realty</span>
             <h1 className='h1 capitalize max-w-[40rem] text-white'>Next Home with Rainbow Rise Realty</h1>
