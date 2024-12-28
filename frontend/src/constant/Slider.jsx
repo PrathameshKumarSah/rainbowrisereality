ae24fe1498b62100ff12f784f975207ce4856ad0
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2024-12-28 21:51:26 +0530

    Update Slider.jsx

--- frontend/src/constant/Slider.jsx
+++ frontend/src/constant/Slider.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 import Gym from '../assets/bow.png'
@@ -46,34 +46,42 @@ const Slider = () => {
         setCurrentIndex(slideIndex);
       };
     
+      // Auto-slide functionality
+useEffect(() => {
+  const interval = setInterval(() => {
+      nextSlide();
+  }, 2000); // 2-second delay
+
+  return () => clearInterval(interval); // Cleanup the interval on unmount
+}, [currentIndex]);
 
   return (
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group overflow-y-hidden'>
     <div
-      className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
     ></div>
     {/* Left Arrow */}
     <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
-      <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
     </div>
     {/* Right Arrow */}
     <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
-      <BsChevronCompactRight onClick={nextSlide} size={30} />
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
     </div>
 
     <div className='flex top-4 justify-center py-2'>
-      {slides.map((slide, slideIndex) => (
-        <div
-          className='text-2xl cursor-pointer'
-          key={slideIndex}
-          onClick={() => goToSlide(slideIndex)}
-        >
-          <RxDotFilled />
-        </div>
-      ))}
+        {slides.map((slide, slideIndex) => (
+            <div
+                className='text-2xl cursor-pointer'
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+            >
+                <RxDotFilled />
+            </div>
+        ))}
     </div>
-  </div>
+    </div>
   )
 }
 
