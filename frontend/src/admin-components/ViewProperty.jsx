ccf1ef8741425daac172a331534c72ce67137e6c
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2024-12-29 12:50:16 +0530

    Update ViewProperty.jsx

--- frontend/src/admin-components/ViewProperty.jsx
+++ frontend/src/admin-components/ViewProperty.jsx
@@ -1,17 +1,16 @@
 import React, {useState, useEffect} from 'react'
-import Axios from 'axios'
 import Item from  "./Item"
-import { ArrowDownNarrowWide, ArrowUpNarrowWide, ChevronDown } from "lucide-react";
-import { BASE_URL } from '../store/apiHandler';
+import { ArrowDownNarrowWide, ArrowUpNarrowWide } from "lucide-react";
+import { apiStore } from '../store/apiHandler';
+import { PuffLoader } from 'react-spinners';
 
 const ViewProperty = () => {
+  const {properties, getProperties, propertyLoading} = apiStore();
     const [propertiesDetails, setPropertiesDetails] = useState([]);
-    const [propertyData, setPropertyData] = useState([]);
+    // const [propertyData, setPropertyData] = useState([]);
 
     const [searchTerm, setSearchTerm] = useState("");
     const [sortOrder, setSortOrder] = useState("asc");
-
-    propertiesDetails.map((item) => console.log(item));
   
     const handleSearch = (e) => {
       setSearchTerm(e.target.value.toLowerCase());
@@ -35,20 +34,24 @@ const ViewProperty = () => {
         property.address.toLowerCase().includes(searchTerm)
     );
 
-    const getData = async () =>{
-      try{
-        const res = await Axios.get(BASE_URL+'/api/properties');
-        setPropertiesDetails(res.data);
-        console.log(res.data);
-        setPropertyData(res.data);
-      }catch{
-        console.log("Error in getting properties details.");
-      }
-    }
-    // getData();
     useEffect(() => {
-      getData();
-    }, [])
+      getProperties();
+      setPropertiesDetails(properties);
+    }, []);
+
+    if(propertyLoading){
+      return (
+        <div className='h-72 flex items-center justify-center'>
+          <PuffLoader 
+          height="80"
+          width="80"
+          radius={1}
+          color="#555"
+          aria-label='puff-loading'
+          />
+        </div>
+      )
+    }
 
   return (
       <div className="container xl:py-6 rounded-3xl">
@@ -89,37 +92,6 @@ const ViewProperty = () => {
           )}
         </div>
 
-        {/* preious and next properties section */}
-        <div className='flex flex-wrap items-center p-3 gap-6 w-full mt-4'>
-          <button className='
-            relative flex items-center py-2 px-3 my-1
-            font-medium rounded-md cursor-pointer
-            transition-colors group
-            bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800'>
-          Previous</button>
-          <div>Page {1} of {2}</div>
-          <button className='
-            relative flex items-center py-2 px-3 my-1
-            font-medium rounded-md cursor-pointer
-            transition-colors group
-            bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800'>
-              Next</button>
-            <div className="mt-2 grid grid-cols-1">
-              <select
-                id="show-page"
-                name="show-page"
-                className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              >
-                <option value={10}>Show 10</option>
-                <option value={20}>Show 20</option>
-              </select>
-              
-              <ChevronDown 
-                aria-hidden="true"
-                className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-              />
-            </div>
-        </div>
 
       </div>
   )
