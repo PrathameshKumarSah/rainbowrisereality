971a3e13ede02e0abc02498e2ca9f5ecb8a5e4d1
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2024-12-29 22:49:57 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '../constant/data'
-import log from '../assets/rw.png'
+import log from '../assets/rrr logo.png'
 import { Mail, PhoneIcon } from 'lucide-react';
 
 const Footer = () => {
