63402b843758940dc4678cf6701416df6e42f9b3
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-01 23:20:20 +0530

    Update WebsiteRoutes.jsx

--- frontend/src/components/WebsiteRoutes.jsx
+++ frontend/src/components/WebsiteRoutes.jsx
@@ -8,29 +8,27 @@ import AboutPage from "../pages/AboutPage"
 import PrivacyPolicy from "../components/PrivacyPolicy"
 import { Toaster } from 'react-hot-toast';
 import Project from "../pages/Project";
-import EnquireModalBox from "./EnquireModalBox";
-import { apiStore } from "../store/apiHandler";
-import ThankYou from "./ThankYou";
-import Property from "../pages/Property";
+import ScrollToTop from "../components/ScrollToTop"
+import Project1 from '../pages/Project1'
+import Project2 from '../pages/Project2'
+import ContactUs from '../pages/ContactUs'
 
 const WebsiteRoutes = () => {
-  const {modalOpen, setModalOpen} = apiStore();
   return (
     <>
         <Header />
+        <ScrollToTop />
         <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/properties">
-              <Route index  element={<Listing />} />
-              <Route path=":propertyId"  element={<Property />} />
-            </Route>
+            <Route path="/properties" element={<Listing />}/>
             <Route path="/about-us" element={<AboutPage />}/>
             <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
             <Route path="/project" element={<Project />}/>
-            <Route path="/thankyou" element={<ThankYou />}/>
+            <Route path="/contactus" element={<ContactUs />}/>
+            <Route path="/project1" element={<Project1 />}/>
+            <Route path="/project2" element={<Project2 />}/>
         </Routes>
         <Toaster />
-        <EnquireModalBox modalOpen={modalOpen} setModalOpen={setModalOpen} />
         <Footer />
     </>
   );
