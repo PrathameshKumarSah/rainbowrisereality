7e4df5b9549519198968246203e732cdad1a5f87
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-01 22:12:37 +0530

    Update WebsiteRoutes.jsx

--- frontend/src/components/WebsiteRoutes.jsx
+++ frontend/src/components/WebsiteRoutes.jsx
@@ -8,19 +8,29 @@ import AboutPage from "../pages/AboutPage"
 import PrivacyPolicy from "../components/PrivacyPolicy"
 import { Toaster } from 'react-hot-toast';
 import Project from "../pages/Project";
+import EnquireModalBox from "./EnquireModalBox";
+import { apiStore } from "../store/apiHandler";
+import ThankYou from "./ThankYou";
+import Property from "../pages/Property";
 
 const WebsiteRoutes = () => {
+  const {modalOpen, setModalOpen} = apiStore();
   return (
     <>
         <Header />
         <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/properties" element={<Listing />}/>
+            <Route path="/properties">
+              <Route index  element={<Listing />} />
+              <Route path=":propertyId"  element={<Property />} />
+            </Route>
             <Route path="/about-us" element={<AboutPage />}/>
             <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
             <Route path="/project" element={<Project />}/>
+            <Route path="/thankyou" element={<ThankYou />}/>
         </Routes>
         <Toaster />
+        <EnquireModalBox modalOpen={modalOpen} setModalOpen={setModalOpen} />
         <Footer />
     </>
   );
