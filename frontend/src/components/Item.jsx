31779e0b663ebf148f271ee8dc9c5470838782cc
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-01 22:15:13 +0530

    Update Item.jsx

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { BASE_URL } from '../store/apiHandler';
-import { ADMIN_BASE_URL } from '../App';
-
+import { apiStore, BASE_URL } from '../store/apiHandler';
+import { useNavigate } from 'react-router-dom';
+import { Bath, Bed, CarFront, Ruler } from 'lucide-react';
 
 const Item = ({ property }) => {
+  const {modalOpen, setModalOpen} = apiStore();
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    setModalOpen(true);
+  }
+
   return (
-    <div className="rounded-2xl p-5 bg-white">
+    <div onClick={() => navigate(`../properties/${property.p_id}`)} className="rounded-2xl p-5 bg-white">
         <div className="pb-2 relative overflow-hidden rounded-xl">
-            <img src={ BASE_URL + "/imgs/" + property.img} alt={property.title} className='bg-cover rounded-xl ease-in duration-300 hover:scale-110' />
-            {/* like btn */}
-            {/* <div className='absolute top-4 right-6'>
-                <HeartBtn />
-            </div> */}
+            <img src={ BASE_URL + "/imgs/" + property.img } alt={property.title} className='bg-cover rounded-xl ease-in duration-300 hover:scale-110' />
         </div>
         <h5 className="bold-16 my-1 text-secondary">{property.location}</h5>
-        <h4 className="medium-18 line-clamp-3">{property.title}</h4>
+        <h4 className="medium-18 line-clamp-1">{property.title}</h4>
         {/* info */}
-        {/* <div className='flex gap-x-2 py-2'>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedrooms}</div>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBathtub /> {property.facilities.bathrooms}</div>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /> {property.facilities.parkings}</div>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><CgRuler /> 400 </div>
-        </div> */}
-        <p className="pt-2 mb-2">{property.description}</p>
-        <p className="pt-2 mb-2">{property.address}</p>
-        {/* <div className='flexBetween'>
-           
-        </div> */}
-        <div className="bold-18 mb-4">{property.price_title}</div>
-{/*         <Link to={ADMIN_BASE_URL+'/view-properties'} className='btn-secondary rounded-xl shadow-sm mt-5'><button>Contact Us</button></Link> */}
-              <div className='btn-secondary rounded-xl shadow-sm mt-5 text-center hover:bg-indigo-800 cursor-pointer'>Contact Us</div>
+        <div className='flex gap-x-2 py-2 line-clamp-1'>
+          {property.bed && <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-2 font-[400] text-sm'><Bed size={20}/> {property?.bed}</div>}
+          {property.bath && <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-2 font-[400] text-sm'><Bath size={20} /> {property?.bath}</div>}
+          {property.parking && <div className='flexCenter gap-x-2 border-slate-900/50 pr-2 font-[400] text-sm'><CarFront size={20} /> {property?.parking}</div>}              
+          {/* {property.area && <div className='flexCenter gap-x-2 border-slate-900/50 pr-2 font-[400] text-sm'><Ruler size={20} /> {property?.area} </div>} */}
+        </div> 
 
+        <p className="pt-2 mb-2 line-clamp-1">{property.description}</p>
+        <p className="pt-2 mb-2 line-clamp-1">{property.address}</p>
+        
+        <div className="bold-18 mb-4">{property.price_title}</div>
+        <div 
+          className='btn-secondary rounded-xl shadow-sm mt-5 text-center hover:bg-indigo-800 cursor-pointer'>View Details</div>
+          {/* <div 
+          onClick={handleOnClick}
+          className='btn-secondary rounded-xl shadow-sm mt-5 text-center hover:bg-indigo-800 cursor-pointer'>Contact Us</div> */}
+        {/* <EnquireModalBox /> */}
     </div>
   );
 };
