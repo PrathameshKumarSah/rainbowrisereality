3311afbe658646dcf34ae546f51014600e47d48f
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-02 16:01:48 +0530

    Update Sidebar.jsx

--- frontend/src/admin-components/Sidebar.jsx
+++ frontend/src/admin-components/Sidebar.jsx
@@ -63,10 +63,12 @@ export default function Sidebar() {
     <aside className="h-screen">
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
-          <img src={logos} 
-            className={`overflow-hidden transition-all ${
-              expanded ? "w-12" : "w-0"
-            }`} /> 
+          <Link to={'../../'} >
+            <img src={logos} 
+              className={`overflow-hidden transition-all ${
+                expanded ? "w-12" : "w-0"
+              }`} />        
+        </Link>  
           {/* <img
             src="https://img.logoipsum.com/243.svg"
             className={`overflow-hidden transition-all ${
