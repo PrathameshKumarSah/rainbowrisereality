9bd4e5c1b876ec5eb7e91c2ac7105056b1e0d3ec
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-02 19:43:31 +0530

    Update WebsiteRoutes.jsx

--- frontend/src/components/WebsiteRoutes.jsx
+++ frontend/src/components/WebsiteRoutes.jsx
@@ -8,37 +8,31 @@ import AboutPage from "../pages/AboutPage"
 import PrivacyPolicy from "../components/PrivacyPolicy"
 import { Toaster } from 'react-hot-toast';
 import Project from "../pages/Project";
-import EnquireModalBox from "./EnquireModalBox";
-import { apiStore } from "../store/apiHandler";
-import ThankYou from "./ThankYou";
-import Property from "../pages/Property";
 import ScrollToTop from "../components/ScrollToTop"
 import Project1 from '../pages/Project1'
 import Project2 from '../pages/Project2'
 import ContactUs from '../pages/ContactUs'
+import Alpha from '../pages/alphabeta'
+import Alpha1 from '../pages/alphabeta1'
 
 const WebsiteRoutes = () => {
-  const {modalOpen, setModalOpen} = apiStore();
   return (
     <>
         <Header />
         <ScrollToTop />
         <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/properties">
-              <Route index  element={<Listing />} />
-              <Route path=":propertyId"  element={<Property />} />
-            </Route>
+            <Route path="/properties" element={<Listing />}/>
             <Route path="/about-us" element={<AboutPage />}/>
             <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
             <Route path="/project" element={<Project />}/>
             <Route path="/contactus" element={<ContactUs />}/>
-            <Route path="/thankyou" element={<ThankYou />}/>
+            <Route path="/alpha" element={<Alpha/>}/>
+            <Route path="/alpha1" element={<Alpha1/>}/>
             <Route path="/project1" element={<Project1 />}/>
             <Route path="/project2" element={<Project2 />}/>
         </Routes>
         <Toaster />
-        <EnquireModalBox modalOpen={modalOpen} setModalOpen={setModalOpen} />
         <Footer />
     </>
   );
