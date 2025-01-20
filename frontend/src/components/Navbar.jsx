87a1edf228312548bb7f5ca2fbc548e3c00600d7
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-20 23:55:11 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,35 +5,41 @@ import {RiCheckboxMultipleBlankFill} from "react-icons/ri"
 
 
 const Navbar = ({ containerStyles }) => {
+  
 
   return (
     <nav className={`${containerStyles}`}>
         <NavLink to={"/"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdHomeWork />
-          <div>Home</div>
+          <div className='font-bold text-lg'>Home</div>
         </NavLink>
         <NavLink to={"/properties"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <RiCheckboxMultipleBlankFill />
-          <div>Properties</div>
+          <div className='font-bold text-lg'>Properties</div>
         </NavLink>
         <NavLink to={"/about-us"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdPermContactCalendar />
-          <div>About Us</div>
+          <div className='font-bold text-lg'>About Us</div>
+        </NavLink>
+        <NavLink to={"/allproject"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
+          <MdHomeWork />
+          <div className='font-bold text-lg'>Projects</div>
         </NavLink>
          <NavLink to={"/godrej"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdHomeWork />
-          <div>Godrej</div>
+          <div className='font-bold text-lg'>Godrej</div>
         </NavLink>
         <NavLink to={"/ska"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdHomeWork />
-          <div>SKA</div>
+          <div className='font-bold text-lg'>SKA</div>
         </NavLink>
         <NavLink to={"/three-page"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdHomeWork />
-          <div>Three Page</div>
+          <div className='font-bold text-lg'>Three Page</div>
         </NavLink>
     </nav>
   )
 }
 
 export default Navbar
+
