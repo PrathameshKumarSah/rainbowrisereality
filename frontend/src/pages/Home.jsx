5894d0fb2cc185cf549c48cbdb329325f37287c9
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-20 18:01:40 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
   return (
     <main>
       <Hero/>
+      <Properties />
       <BuilderAssociations />
       <About/>
-      <Properties />
       <LatestProjects />
       <Slider />
       <Blogs />
