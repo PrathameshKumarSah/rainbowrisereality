a824fa0d6749affad1201f5e61fa60c818ac4c13
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-21 00:03:47 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,16 +14,16 @@ const Home = () => {
   return (
     <main>
       <Hero/>
-      <Properties />
       <BuilderAssociations />
-      <About/>
+      <Properties />
+{/*       <About/> */}
       <LatestProjects />
       <Slider />
-      <Blogs />
-      <div className=' py-16 overflow-x-hidden'>
+{/*       <div className=' py-16 overflow-x-hidden'>
         <img src={bannerImg} alt="" />
-      </div>
+      </div> */}
       <ReadyToMoveProjects />
+      <Blogs />
          <div class="fixed bottom-4 right-4">
            <button className="flexCenter gap-x-6 medium-10 rounded-full pr-3">
                              <a href="https://wa.me/8058517274" target="_blank"><img src={userIcon} alt="" height={40} width={40} /></a>
