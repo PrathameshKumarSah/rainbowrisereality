c9a95ecb74948c16f6276205aae7344e3475b7a5
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-20 23:59:05 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -1,31 +1,83 @@
-import React from 'react'
-import { BLOGS } from '../constant/data'
+import React from "react";
 
 const Blogs = () => {
   return (
-    <section className='max-padd-container'>
-      <div className='max-padd-container py-10 xl:py-20 rounded-3xl'>
-        <span className='medium-18'>Stay Updated with the Latest News!</span>
-        <h2 className='h1'>Our Expert </h2>
-        {/* container */}
-        <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-20'>
-          {BLOGS.map((blog)=> (
-            <div key={blog.title} className='rounded-3xl border-[11px] border-primary shadow-sm overflow-hidden relative'>
-              <img src={blog.image} alt="" />
-              {/* overlay */}
-              <div className='absolute top-0 left-0 h-full w-full bg-black/25'></div>
-              {/* info */}
-              <div className='absolute bottom-4 left-4 text-white text-[15px]'>
-              <h3 className='font-[600] text-[16px] pr-4 leading-5'>{blog.title}</h3>
-              <h4 className='medium-14 pb-3 pt-1'>{blog.category}</h4>
-              <button className='bg-[#0A3981] rounded-xl font-semibold text-white px-3 py-1'>Visit</button>
+    <>
+      <section className="max-padd-container bg-white pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                <span className="mb-2 block text-xl font-bold text-black">
+                  Our Blogs
+                </span>
+                <h2 className="mb-4 text-3xl font-bold text-secondary  sm:text-4xl md:text-[40px]">
+                  Our Recent News
+                </h2>
+                <p className="text-base text-body-color dark:text-dark-6">
+                  There are many variations of passages of Lorem Ipsum available
+                  but the majority have suffered alteration in some form.
+                </p>
               </div>
             </div>
-          ))}
+          </div>
+
+          <div className="-mx-4 flex flex-wrap">
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              image="https://i.ibb.co/Cnwd4q6/image-01.jpg"
+            />
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              image="https://i.ibb.co/Y23YC07/image-02.jpg"
+            />
+            <BlogCard
+              date="Dec 22, 2023"
+              CardTitle="Meet AutoManage, the best AI management tools"
+              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              image="https://i.ibb.co/7jdcnwn/image-03.jpg"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Blogs;
+
+const BlogCard = ({ image, date, CardTitle, CardDescription }) => {
+  return (
+    <>
+      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+        <div className="mb-10 w-full">
+          <div className="mb-8 overflow-hidden rounded">
+            <img src={image} alt="" className="w-full" />
+          </div>
+          <div>
+            {date && (
+              <span className="mb-5 inline-block rounded bg-secondary px-4 py-1 text-center text-base font-semibold leading-loose text-white">
+                {date}
+              </span>
+            )}
+            <h3>
+              <a
+                href="/#"
+                className="mb-4 inline-block text-xl font-bold text-black hover:text-secondary  sm:text-2xl lg:text-xl xl:text-2xl"
+              >
+                {CardTitle}
+              </a>
+            </h3>
+            <p className="text-base text-body-color dark:text-dark-6">
+              {CardDescription}
+            </p>
+          </div>
         </div>
       </div>
-    </section>
-  )
-}
-
-export default Blogs
+    </>
+  );
+};
