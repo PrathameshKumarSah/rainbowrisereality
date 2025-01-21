5e1885bd11793f143f7f5b2426b431d7e5b01124
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-22 00:29:56 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,94 +1,42 @@
-import { Search } from "lucide-react";
-import React, { useState, useRef, useEffect } from "react";
-
-const HeroSearchBar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("Residential");
-  const dropdownRef = useRef(null);
-
-  const handleOutsideClick = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setDropdownOpen(false);
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(e.target[1].value===''){ return ;}
-    alert("clicked search");
-    console.log(e.target[1].value);
-    e.target[1].value = '';
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, []);
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import Inquiry from '../components/InquiryBox'
+import { ArrowRight } from 'lucide-react'
+import VideoSource from '../assets/vack.mp4' // Import your video file
+import HeroSearchBar from './HeroSearchBar'
+const Hero = () => {
   return (
-      <form onSubmit={handleSubmit} className="flex gap-1 items-center w-full md:w-3/4 lg:w-2/3 shadow-md text-sm md:text-base">
-        {/* Dropdown */}
-        <div className="relative w-1/3" ref={dropdownRef}>
-          <button
-            className="w-full px-1 md:px-4 py-3 md:py-4 text-left bg-white rounded hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-800 flex justify-between items-center"
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-          >
-            <span className="font-bold">{selectedOption}</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-5 w-5 transition-transform ${dropdownOpen ? "rotate-180" : "rotate-0"}`}
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 011.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.23 8.27a.75.75 0 01.02-1.06z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-          {dropdownOpen && (
-            <ul className="absolute z-10 mt-1 w-full border bg-white border-gray-200 rounded-md shadow-lg">
-              <li
-                className="px-2 md:px-4 py-2 hover:bg-gray-500 hover:text-white cursor-pointer border-b rounded-md border-2"
-                onClick={() => {
-                  setSelectedOption("Residential");
-                  setDropdownOpen(false);
-                }}
-              >
-                Residential
-              </li>
-              <li
-                className="px-2 md:px-4 py-2 hover:bg-gray-500 hover:text-white cursor-pointer rounded-md border-2"
-                onClick={() => {
-                  setSelectedOption("Commercial");
-                  setDropdownOpen(false);
-                }}
-              >
-                Commercial
-              </li>
-            </ul>
-          )}
-        </div>
-
-        {/* Search Input */}
-        <input
-          type="text"
-          className="w-full px-4 py-3 md:py-4 border-l placeholder-black border-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
-          placeholder="Search projects or properties..."
+    <section className='w-full'>
+      {/* < Inquiry/> */}
+      <div className='relative w-full h-[685px]'>
+        {/* Background video */}
+        <video
+          className='absolute top-0 left-0 w-full h-full object-cover'
+          src={VideoSource}
+          autoPlay
+          loop
+          muted
         />
-
-        {/* Search Button */}
-        <button
-          className="flex gap-2 px-2 md:px-6 py-3 md:py-4 text-white font-medium rounded bg-blue-900 transition duration-300 hover:bg-blue-950 hover:-translate-y-1"
-        >
-        <Search size={20} />
-          <span className="hidden font-bold md:inline">SEARCH</span>
-        </button>
-      </form>
-  );
-};
-
-export default HeroSearchBar;
+        {/* Content overlay */}
+        <div className='relative top-52 sm:pl-10 z-10'>
+          <span className='medium-18 text-white'>Welcome to Rainbow Rise Realty</span>
+          <div className="items-center justify-center overflow-y-hidden overflow-x-hidden mt-4">
+            <div className="w-max">
+              <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 h1 capitalize text-white pr-5 text-2xl font-bold">
+                Next Home with Rainbow <br /> Rise Realty
+              </h1>
+            </div>
+          </div>
+          <p className='my-10 max-w-[33rem] text-white'>
+            Luxury, comfort, and convenience—properties that match your lifestyle. Invest in your future with properties designed to inspire and grow.
+          </p>
+          {/* button */}
+          <HeroSearchBar />
+        </div>
+        {/* Overlay to dim video for better text contrast */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30 z-0"></div>
+      </div>
+    </section>
+  )
+}
+export default Hero
