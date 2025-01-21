c3badc5d1b992c59a2c32017fafb2bdb52c30fcd
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-22 02:29:34 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,17 +13,17 @@ const Navbar = ({ containerStyles }) => {
           <MdHomeWork />
           <div className='font-bold text-lg'>Home</div>
         </NavLink>
-        <NavLink to={"/properties"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
+        <NavLink to={"/allproject"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <RiCheckboxMultipleBlankFill />
-          <div className='font-bold text-lg'>Properties</div>
+          <div className='font-bold text-lg'>Projects</div>
         </NavLink>
         <NavLink to={"/about-us"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdPermContactCalendar />
           <div className='font-bold text-lg'>About Us</div>
         </NavLink>
-        <NavLink to={"/allproject"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
+        <NavLink to={"/properties"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
           <MdHomeWork />
-          <div className='font-bold text-lg'>Projects</div>
+          <div className='font-bold text-lg'>Properties</div>
         </NavLink>
     </nav>
   )
