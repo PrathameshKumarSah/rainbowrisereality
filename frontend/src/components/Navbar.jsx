a6d81d4e1c07257c09e6904439a79087c026e0dd
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-22 02:31:08 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,13 +17,13 @@ const Navbar = ({ containerStyles }) => {
           <RiCheckboxMultipleBlankFill />
           <div className='font-bold text-lg'>Projects</div>
         </NavLink>
-        <NavLink to={"/about-us"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
-          <MdPermContactCalendar />
-          <div className='font-bold text-lg'>About Us</div>
-        </NavLink>
         <NavLink to={"/properties"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
-          <MdHomeWork />
+          <MdPermContactCalendar />
           <div className='font-bold text-lg'>Properties</div>
+        </NavLink>
+        <NavLink to={"/about-us"} className={({isActive})=> isActive ? "active-link flexCenter gap-x-1 rounded-full p-2" : "flexCenter gap-x-1 rounded-full px-2 py-1"}>
+          <MdHomeWork />
+          <div className='font-bold text-lg'>About Us</div>
         </NavLink>
     </nav>
   )
