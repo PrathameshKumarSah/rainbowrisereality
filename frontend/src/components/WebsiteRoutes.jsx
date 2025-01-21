63b6e6d29ce6638b39638fb19f628d4e589ef8c5
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-22 00:44:25 +0530

    Update WebsiteRoutes.jsx

--- frontend/src/components/WebsiteRoutes.jsx
+++ frontend/src/components/WebsiteRoutes.jsx
@@ -7,14 +7,13 @@ import Header from "../components/Header"
 import AboutPage from "../pages/AboutPage"
 import PrivacyPolicy from "../components/PrivacyPolicy"
 import { Toaster } from 'react-hot-toast';
-import Project from "../pages/Project";
 import EnquireModalBox from "./EnquireModalBox";
 import { apiStore } from "../store/apiHandler";
 import ThankYou from "./ThankYou";
 import Property from "../pages/Property";
 import ScrollToTop from "../components/ScrollToTop"
-import Project1 from '../pages/Project1'
-import Project2 from '../pages/Project2'
+import Residential from '../pages/Residential'
+import Commercial from '../pages/Commercial'
 import ContactUs from '../pages/ContactUs'
 import Alpha from '../pages/alphabeta'
 import Alpha1 from '../pages/alphabeta1'
@@ -34,11 +33,10 @@ const WebsiteRoutes = () => {
             </Route>
             <Route path="/about-us" element={<AboutPage />}/>
             <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
-            <Route path="/godrej" element={<Project />}/>
             <Route path="/contactus" element={<ContactUs />}/>
             <Route path="/thankyou" element={<ThankYou />}/>
-            <Route path="/ska" element={<Project1 />}/>
-            <Route path="/three-page" element={<Project2 />}/>
+            <Route path="/residential" element={<Residential />}/>
+            <Route path="/commercial" element={<Commercial />}/>
            <Route path="/alpha" element={<Alpha/>}/>
             <Route path="/alpha1" element={<Alpha1/>}/>
           <Route path="/allproject" element={<Allproject />}/>
