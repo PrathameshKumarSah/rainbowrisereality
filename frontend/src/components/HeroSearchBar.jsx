0a287134d9ee7eeb899996b10b1b8e826094b5ac
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-21 15:41:09 +0530

    Update HeroSearchBar.jsx

--- frontend/src/components/HeroSearchBar.jsx
+++ frontend/src/components/HeroSearchBar.jsx
@@ -85,7 +85,7 @@ const HeroSearchBar = () => {
           className="flex gap-2 px-2 md:px-6 py-3 text-white font-medium rounded bg-blue-900 transition duration-300 hover:bg-blue-950 hover:-translate-y-1"
         >
         <Search size={20} />
-          <span className="hidden md:inline">SEARCH</span>
+          <span className="hidden md:inline">Search</span>
         </button>
       </form>
   );
