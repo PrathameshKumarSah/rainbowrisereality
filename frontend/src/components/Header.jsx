f211f1f8dced8ae59623a770e3b366c4674e5b5d
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-23 18:27:22 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
       };
   }, [menuOpened]);
   return (
-   <header className="fixed top-2 w-full left-0 right-0 z-50 sm:w-full">
+   <header className="absolute top-2 w-full left-0 right-0 z-50 sm:w-full">
       {/* container */}
       <div className={`${active ? "py-0" : "py-1"}max-padd-container  transition-all duration-200 rounded-full px-6  ring-slate-900/5`}>
         <div className="flexBetween py-3">
@@ -58,9 +58,9 @@ const Header = () => {
             {/* Navbar */}
             <div className="flexCenter gap-x-4 ">
                 {/* Desktop */}
-                <Navbar containerStyles={"hidden 2xl:flex gap-x-5 xl:gap-x-10 capitalize text-bolder  medium-15 ring-slate-900/10 rounded-full p-2"} />
+                <Navbar containerStyles={"hidden 2xl:flex gap-x-5 xl:gap-x-10 capitalize text-bolder  medium-15 ring-slate-900/10 rounded-full p-2 bg-primary "} />
                 {/* Mobile */}
-                <Navbar containerStyles={` mobile-navbar ${menuOpened ? "flex items-start flex-col gap-y-8 capitalize fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration=300 z-50" : "flex items-start flex-col gap-y-8 capitalize fixed top-20 -right-[100%] p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration=300 z-50"}`} />
+                <Navbar containerStyles={` mobile-navbar ${menuOpened ? "flex items-start flex-col gap-y-8 capitalize fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-54 medium-16 ring-1 ring-slate-900/5 transition-all duration=300 z-50" : "flex items-start flex-col gap-y-8 capitalize fixed top-20 -right-[100%] p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration=300 z-50"}`} />
             </div>
             {/* button */}
             <div className="flexBetween gap-x-3 sm:gap-x-5 bold-16">
