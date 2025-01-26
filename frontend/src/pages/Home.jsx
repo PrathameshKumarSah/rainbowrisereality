990bd60be5f91049eb851d29b39a819ca7df8cfa
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-26 12:23:19 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Properties from '../components/Properties'
 import Blogs from '../components/Blogs'
-import bannerImg from '../assets/banner.png'
 import BuilderAssociations from '../components/BuilderAssociations'
-import LatestProjects from '../components/LatestProjects'
-import ReadyToMoveProjects from '../components/ReadyToMoveProjects'
 
 const Home = () => {
   return (
@@ -14,12 +11,8 @@ const Home = () => {
       <Hero/>
       <Properties />
       <BuilderAssociations />
-{/*       <About/> */}
+      <About/>
 {/*       <LatestProjects /> */}
-{/*       <div className=' py-16 overflow-x-hidden'>
-        <img src={bannerImg} alt="" />
-      </div> */}
-{/*       <ReadyToMoveProjects /> */}
       <Blogs />
     </main>
   )
