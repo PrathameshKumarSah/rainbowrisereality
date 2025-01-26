d21d59a1453df0424ec1a1ca76686a6f64ff3fdd
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-27 00:11:39 +0530

    Update web.routes.js

--- backend/src/routes/web.routes.js
+++ backend/src/routes/web.routes.js
@@ -1,33 +1,63 @@
 import express from "express";
-import { showProperties, handleLogin, addPropertyHandler, getProperty, updateImg, updatePropertyDetails, latestProperty, removeProperty, logout, updateProfileDetails, changePassword, sendOTP, otpVerification, createNewPass, protectRoute, sendEnquire, sendContactMsg } from "../controllers/api.js";
+import { showProperties, handleLogin, addPropertyHandler, getProperty, updateImg, updatePropertyDetails, latestProperty, removeProperty, logout, updateProfileDetails, changePassword, sendOTP, otpVerification, createNewPass, protectRoute, sendEnquire, sendContactMsg, addProjectHandler } from "../controllers/api.js";
 import multer from 'multer';
-import path from 'path';
+import fs from 'fs';
 
 
 const router = express.Router();
 
-// Define storage configuration for multer ===========================
-export const storage = multer.diskStorage({
+// Create directories for uploads if they don't exist
+const createDirectories = () => {
+  const directories = ["uploads/images", "uploads/brochure"];
+  directories.forEach((dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  });
+};
+createDirectories();
+
+// Configure Multer storage
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './imgs'); // Directory to save uploadImged files
+    if (file.fieldname === "images") {
+      cb(null, "uploads/images");
+    } else if (file.fieldname === "brochure") {
+      cb(null, "uploads/brochure");
+    } else {
+      // console.log(file.fieldname);
+      cb(null, "uploads/images");
+    }
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext); 
-  }
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
-export const uploadImg = multer({ storage });
+// Multer Middleware
+export const upload = multer({ storage });
+
+// const uploadHandler = upload.fields([
+//   { name: "images", maxCount: 6 }, // Multiple files from 'multipleFiles' field
+//   { name: "brochure", maxCount: 1 }, // Single file from 'singleFile' field
+// ]);
+
+// Define file upload fields
+const uploadFields = upload.fields([
+  { name: "images", maxCount: 10 },
+  { name: "brochure", maxCount: 1 },
+]);
+
 
 router.get("/properties", showProperties);
 router.post("/login", handleLogin);
 router.get("/latest-property",  latestProperty);
-router.post("/add-property", uploadImg.single('img'), addPropertyHandler);
+router.post("/add-property", upload.single('img'), addPropertyHandler);
+router.post("/add-project", uploadFields, addProjectHandler);
 router.get("/get-property/:id", getProperty);
 router.get("/auth-check", protectRoute);
 router.get("/remove-property/:id", removeProperty);
-router.post("/update-img", uploadImg.single('img'), updateImg);
+router.post("/update-img", upload.single('img'), updateImg);
 router.post("/update-details",  updatePropertyDetails);
 router.post("/change-password",  changePassword);
 router.post("/update-profile",  updateProfileDetails);
