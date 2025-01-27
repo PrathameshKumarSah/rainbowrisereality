93452417a0b0a2781bcf94ab2f136f44f2339a66
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-27 17:35:52 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Properties from '../components/Properties'
-import Blogs from '../components/Blogs'
+// import Blogs from '../components/Blogs'
 import BuilderAssociations from '../components/BuilderAssociations'
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
       <BuilderAssociations />
       <About/>
 {/*       <LatestProjects /> */}
-      <Blogs />
+{/*       <Blogs /> */}
     </main>
   )
 }
