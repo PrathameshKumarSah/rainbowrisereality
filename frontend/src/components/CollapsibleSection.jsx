3fd4e6a584f29f0530b2b715cd9c1c30cf014e87
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-27 23:40:03 +0530

    Update CollapsibleSection.jsx

--- frontend/src/components/CollapsibleSection.jsx
+++ frontend/src/components/CollapsibleSection.jsx
@@ -1,23 +1,24 @@
+import { MinusCircle, PlusCircle } from "lucide-react";
 import React, { useState } from "react";
 
 const CollapsibleSection = ({ title, children }) => {
     const [isOpen, setIsOpen] = useState(true);
   
     return (
-      <div className="border-b border-gray-200 py-4">
+      <div className="border-b border-gray-200 my-4 shadow-lg">
         <div
-          className="flex justify-between items-center cursor-pointer"
+          className="flex justify-between items-center cursor-pointer bg-slate-200 px-4 transition-all duration-300"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <h2 className="text-2xl font-medium text-black">{title}</h2>
+          <h2 className="text-2xl p-2 font-medium text-black">{title}</h2>
           <button
-            className="text-gray-600 focus:outline-none"
+            className="text-orange-500 focus:outline-none"
             aria-label={isOpen ? "Collapse section" : "Expand section"}
           >
-            {isOpen ? "-" : "+"}
+            {isOpen ? <MinusCircle /> : <PlusCircle />}
           </button>
         </div>
-        {isOpen && <div className="mt-4">{children}</div>}
+        {isOpen && <div className="p-4 bg-white text-black">{children}</div>}
       </div>
     );
   };
