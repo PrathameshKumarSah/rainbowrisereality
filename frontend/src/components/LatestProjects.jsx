230ba64e3ef4c0305fb69ba67cb5529774a5122d
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-27 19:16:27 +0530

    Update LatestProjects.jsx

--- frontend/src/components/LatestProjects.jsx
+++ frontend/src/components/LatestProjects.jsx
@@ -1,77 +1,58 @@
-import React from 'react'
-import Spa from '../assets/ska.jpg'
-import Sunshine from '../assets/god.jpg'
-import Three from '../assets/page.jpg'
+import React from "react";
 
 const LatestProjects = () => {
   return (
-    <div>
-        <h2 className=" max-padd-container mt-10 text-center text-4xl md:text-5xl font-extrabold text-[#000000] overflow-y-hidden mb-10">
-        Our latest Projects
-      </h2>
-      <section className="max-padd-container container mx-auto p-6 mt-6">
-        {/* Property 1: Rise Resort Residences */}
-        <div className="flex flex-col md:flex-row items-center mb-12">
-          {/* Image */}
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <img
-              src={Sunshine}
-              alt="Rise Resort Residences"
-              className="w-full h-auto object-cover rounded-lg"
-            />
-          </div>
-          {/* Content */}
-          <div className="w-full md:w-1/2 md:pl-8 text-left">
-            <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center">
-              GODREJ
-            </h2>
-            <p className="text-gray-600 leading-relaxed">
-            Godrej Properties Noida is a prestigious real estate development offering modern homes with world-class amenities and sustainable designs. Located in prime areas of Noida, these properties blend luxury, comfort, and innovation. With a reputation for quality and timely delivery, Godrej Properties ensures an exceptional lifestyle in one of India's fastest-growing cities.
-            </p>
-            
-          </div>
+   <div className="max-padd-container h-full w-full bg-gray-800 pt-12 p-4">\<div className="text-center mb-8">
+    <h2 className="text-2xl font-bold text-white">WHY CHOOSE US?</h2>
+    <p className="text-white mt-2">
+      With years of industry experience, our team of experts understands the nuances of the real estate market, ensuring that you make informed decisions.
+    </p>
+  </div>
+    <div className="grid gap-14 md:grid-cols-3 md:gap-5">
+      
+      <div className="rounded-xl bg-white p-6 text-center shadow-xl">
+        <div
+          className="mx-auto flex h-16 w-16 -translate-y-12 transform items-center justify-center rounded-full bg-teal-400 shadow-lg shadow-teal-500/40">
+         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd"/>
+</svg>
+
         </div>
-       
-        {/* Property 2: M3M The Cullinan */}
-        <div className="flex flex-col-reverse md:flex-row items-center mb-10">
-          {/* Content */}
-          <div className="w-full md:w-1/2 md:pr-8 text-left">
-            <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center">
-              SKA DESTINY ONE
-            </h2>
-            <p className="text-gray-600 leading-relaxed">
-            SKA Destiny One, developed by SKA Group, is a premium residential project located in Sector ZETA 1, Greater Noida. Spanning approximately 6 acres, it offers luxurious 3 and 4 BHK apartments designed to provide a lavish lifestyle. The project boasts world-class amenities, including a swimming pool, clubhouse, gym, and indoor games facilities, ensuring residents enjoy both comfort and convenience.
-            </p>
-          </div>
-          {/* Image */}
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <img
-              src={Spa}
-              alt="M3M The Cullinan"
-              className="w-full h-auto object-cover rounded-lg"
-            />
-          </div>
+        <h1 className="text-darken mb-3 text-2xl font-medium lg:px-14">Customers</h1>
+        <p className="px-4 text-gray-500">Satisfied customers who keep re-imposing their faith in us by giving us repeat orders.</p>
+      </div>
+      <div data-aos-delay="150" className="rounded-xl bg-white p-6 text-center shadow-xl">
+        <div
+          className="mx-auto flex h-16 w-16 -translate-y-12 transform items-center justify-center rounded-full shadow-lg bg-rose-500 shadow-rose-500/40">
+        <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"  id="Expert">
+  <path d="M47.0165,5.09429c-2.77576,0-5.36877,.77136-7.60114,2.091C22.13173-.39247,1.71028,13.15453,2.0168,32.09459c1.48193,35.81702,52.52124,35.81006,53.99969-.00031,11.3797-8.6134,5.44879-26.89911-9-27ZM29.0165,57.09429c-13.78516,0-25-11.21484-25-25C3.68813,15.1483,21.65016,2.56371,37.42891,8.57066c-1.26624,1.05524-2.34418,2.32233-3.20746,3.73505-7.70532-3.94086-17.36127,2.10785-17.20483,10.78876-.00012,5.58978,3.8299,10.28979,8.99988,11.61981v.70001c-6.27997,1.38-11,6.98999-11,13.67999,0,.54999,.45001,1,1,1h26c.54999,0,1-.45001,1-1,0-6.69-4.71997-12.29999-11-13.67999v-.70001c2.08093-.53534,3.94019-1.62024,5.42303-3.08673,4.28796,3.77747,11.48279,4.58398,16.54468,1.73877-.66455,13.19641-11.60913,23.72797-24.96771,23.72797Zm0-24c-5.51001,0-10-4.48999-10-10-.17279-7.16425,7.91217-12.2597,14.26929-9.01703-2.44434,5.20068-1.20386,12.12329,2.69745,16.14404-1.80261,1.76605-4.25195,2.87299-6.96674,2.87299Zm1,2v1c0,.54999-.45001,1-1,1s-1-.45001-1-1v-1h2Zm27.12109-15.87891l-9,9c-1.11646,1.1637-3.12573,1.16412-4.24219-.00006,0,.00006-5-4.99994-5-4.99994-2.72296-2.86761,1.37146-6.96729,4.24225-4.24213-.00006-.00006,2.87885,2.87885,2.87885,2.87885l6.87891-6.87891c2.86926-2.72418,6.96643,1.37323,4.24219,4.24219Z" fill="#ffffff" class="color000000 svgShape"></path>
+</svg>
         </div>
-        <div className="flex flex-col md:flex-row items-center mb-12 mt-10">
-          {/* Image */}
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <img
-              src={Three}
-              alt="Rise Resort Residences"
-              className="w-full h-auto object-cover rounded-lg"
-            />
-          </div>
-          {/* Content */}
-          <div className="w-full md:w-1/2 md:pl-8 text-left">
-            <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center">
-              Three Page
-            </h2>
-            <p className="text-gray-600 leading-relaxed">
-            Page Three Residences by H&S is a luxury residential project located in Jaypee Greens, Greater Noida. Spanning approximately 1.28 acres, it offers 4 and 5 BHK apartments, presidential penthouses, and duplex villas, totaling around 46 units. The development boasts modern amenities, including a spa, restaurant, cycling and jogging tracks, gymnasium, and swimming pool.
-            </p>
-          </div>
+        <h1 className="text-darken mb-3 text-2xl font-medium lg:px-14 ">Experts</h1>
+        <p className="px-4 text-gray-500"> Highly qualified and trained real estate experts, who are always ready to service and give their best to our customers.</p>
+      </div>
+      <div data-aos-delay="300" className="rounded-xl bg-white p-6 text-center shadow-xl">
+        <div
+          className="mx-auto flex h-16 w-16 -translate-y-12 transform items-center justify-center rounded-full shadow-lg bg-sky-500 shadow-sky-500/40">
+         <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" viewBox="0 0 32 32" id="Partnership">
+  <path fill="#ffffff" d="M21.5,4a3.492,3.492,0,0,0,6.65,1.5h.01A3.406,3.406,0,0,0,28.5,4a3.5,3.5,0,0,0-6.69-1.43A3.432,3.432,0,0,0,21.5,4Z" class="colorffd9aa svgShape"></path>
+  <path fill="#595959" d="M21.81,2.57l4.69.93v2h1.66A3.406,3.406,0,0,0,28.5,4a3.5,3.5,0,0,0-6.69-1.43Z" class="color57565d svgShape"></path>
+  <rect width="3" height="4" x="23.5" y="6.5" fill="#ffffff" class="colorffd9aa svgShape"></rect>
+  <path fill="#7a7a7a" d="M22.5,18.5l.881,11.447A1.682,1.682,0,0,0,25.057,31.5h0a1.682,1.682,0,0,0,1.662-1.426L28.5,18.5" class="color416fb3 svgShape"></path>
+  <path fill="#a7a7a7" d="M15.5,14A1.5,1.5,0,0,0,17,15.5h2.17a4.024,4.024,0,0,0,2.4-.8l.93-.7v4.5h6v-7a2.006,2.006,0,0,0-2-2H24.2a1.986,1.986,0,0,0-1.25.44l-3.2,2.56H17A1.5,1.5,0,0,0,15.5,14Z" class="colorfbde53 svgShape"></path>
+  <path fill="#ffffff" d="M15.5 14A1.5 1.5 0 0 0 17 15.5h1.5v-3H17A1.5 1.5 0 0 0 15.5 14zM10.5 4A3.492 3.492 0 0 1 3.85 5.5H3.84A3.406 3.406 0 0 1 3.5 4a3.5 3.5 0 0 1 6.69-1.43A3.432 3.432 0 0 1 10.5 4z" class="colorffd9aa svgShape"></path>
+  <path fill="#595959" d="M10.19,2.57,5.5,3.5v2H3.84A3.406,3.406,0,0,1,3.5,4a3.5,3.5,0,0,1,6.69-1.43Z" class="color57565d svgShape"></path>
+  <rect width="3" height="4" x="5.5" y="6.5" fill="#ffffff" class="colorffd9aa svgShape"></rect>
+  <path fill="#7a7a7a" d="M9.5,18.5,8.619,29.947A1.682,1.682,0,0,1,6.943,31.5h0a1.682,1.682,0,0,1-1.662-1.426L3.5,18.5" class="color416fb3 svgShape"></path>
+  <path fill="#999999" d="M16.5,14A1.5,1.5,0,0,1,15,15.5H12.83a4.024,4.024,0,0,1-2.4-.8L9.5,14v4.5h-6v-7a2.006,2.006,0,0,1,2-2H7.8a1.986,1.986,0,0,1,1.25.44l3.2,2.56H15A1.5,1.5,0,0,1,16.5,14Z" class="colored456a svgShape"></path>
+  <path fill="#ffffff" d="M16.5,14A1.5,1.5,0,0,1,15,15.5H13.5v-3H15A1.5,1.5,0,0,1,16.5,14Z" class="colorffd9aa svgShape"></path>
+  <path d="M27,9.05V7.5a.472.472,0,0,0-.01-.051A4,4,0,0,0,28.6,5.707l.009-.017A3.956,3.956,0,0,0,29,4a3.99,3.99,0,0,0-7.6-1.721c-.009.013-.015.028-.023.042A3.941,3.941,0,0,0,23,7.444V9.308a2.446,2.446,0,0,0-.362.241L19.574,12H17a1.968,1.968,0,0,0-1,.276A1.98,1.98,0,0,0,15,12H12.427L9.36,9.548A2.488,2.488,0,0,0,9,9.309V7.5a.472.472,0,0,0-.01-.051A3.941,3.941,0,0,0,10.618,2.32c-.008-.013-.013-.028-.022-.04A3.987,3.987,0,1,0,3.388,5.69l.009.017A4,4,0,0,0,5.01,7.449.472.472,0,0,0,5,7.5V9.051A2.5,2.5,0,0,0,3,11.5v6.991a.4.4,0,0,0,0,.048c0,.016,0,.03,0,.046L4.786,30.15a2.181,2.181,0,0,0,4.331-.165L10,18.538c0-.006,0-.011,0-.017s0-.013,0-.021V15l.134.1a4.525,4.525,0,0,0,2.7.9H15a1.977,1.977,0,0,0,1-.277A1.968,1.968,0,0,0,17,16h2.17a4.5,4.5,0,0,0,2.7-.9L22,15v3.5c0,.007,0,.013,0,.021s0,.011,0,.017l.88,11.452a2.182,2.182,0,0,0,4.332.156l1.779-11.562L29,18.536c0-.015,0-.03,0-.045V11.5a2.521,2.521,0,0,0-2-2.45ZM4,4A2.991,2.991,0,0,1,9.4,2.217l-4,.793A.5.5,0,0,0,5,3.5V5H4.184A2.965,2.965,0,0,1,4,4Zm.78,2H5.5A.5.5,0,0,0,6,5.5V3.911l3.861-.766A2.959,2.959,0,0,1,10,4,2.989,2.989,0,0,1,4.78,6ZM8,7.858V9.017C7.933,9.012,7.866,9,7.8,9H6V7.858A3.954,3.954,0,0,0,7,8,3.954,3.954,0,0,0,8,7.858Zm.121,22.051A1.189,1.189,0,0,1,6.942,31a1.177,1.177,0,0,1-1.168-1L4.083,19H8.96ZM13,15h-.167a3.524,3.524,0,0,1-2.1-.7L7.8,12.1a.5.5,0,1,0-.6.8l1.851,1.388A.491.491,0,0,0,9,14.5V18H4V11.5A1.5,1.5,0,0,1,5.5,10H7.8a1.5,1.5,0,0,1,.937.329l3.2,2.563a.5.5,0,0,0,.313.109H13ZM25,1a3,3,0,0,1,3,3,2.965,2.965,0,0,1-.184,1H27V3.5a.5.5,0,0,0-.4-.49l-4-.793A2.988,2.988,0,0,1,25,1ZM22,4a2.959,2.959,0,0,1,.139-.855L26,3.911V5.5a.5.5,0,0,0,.5.5h.72A2.989,2.989,0,0,1,22,4Zm4,3.858V9H24.2c-.067,0-.133.012-.2.017V7.858a3.593,3.593,0,0,0,2,0ZM15,15H14V13h1a1,1,0,0,1,0,2Zm3,0H17a.968.968,0,0,1-.253-.047,1.927,1.927,0,0,0,0-1.906A.968.968,0,0,1,17,13h1Zm8.226,14.992a1.182,1.182,0,0,1-2.348-.08L23.04,19h4.877ZM28,18H23V14.5a.492.492,0,0,0-.05-.211L24.8,12.9a.5.5,0,0,0-.6-.8l-2.928,2.2a3.507,3.507,0,0,1-2.1.7H19V13h.75a.5.5,0,0,0,.312-.109l3.2-2.562A1.472,1.472,0,0,1,24.2,10h2.3A1.514,1.514,0,0,1,28,11.5Z" fill="#000000" class="color000000 svgShape"></path>
+</svg>
         </div>
-      </section>
+        <h1 className="text-darken mb-3 pt-2 text-2xl font-medium  lg:px-14">Tie Ups</h1>
+        <p className="px-4 text-gray-500">Partners, who have established themselves as credible and highly acclaimed business houses.</p>
+      </div>
+    </div>
   </div>
   )
 }
