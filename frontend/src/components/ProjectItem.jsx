aa3660f96d5132ffbd50abf40d037ac2b1e8dbc1
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-29 10:23:33 +0530

    Update ProjectItem.jsx

--- frontend/src/components/ProjectItem.jsx
+++ frontend/src/components/ProjectItem.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { apiStore, BASE_URL } from '../store/apiHandler';
+import { BASE_URL } from '../store/apiHandler';
 import { useNavigate } from 'react-router-dom';
-import { Bath, Bed, CarFront, Ruler } from 'lucide-react';
+import { Bed, MapPin, Ruler } from 'lucide-react';
 
 const ProjectItem = ({ property }) => {
-//   const {modalOpen, setModalOpen} = apiStore();
   const navigate = useNavigate();
-//   const handleOnClick = () => {
-//     setModalOpen(true);
-//   }
 
   return (
     <div onClick={() => navigate(`../project/${property.id}`)} className="rounded-2xl p-5 bg-white">
@@ -21,20 +17,15 @@ const ProjectItem = ({ property }) => {
         {/* info */}
         <div className='flex gap-x-2 py-2 line-clamp-1'>
           {property.rooms && <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-2 font-[400] text-sm'><Bed size={20}/> {property?.rooms}</div>}
-          {property.bathrooms && <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-2 font-[400] text-sm'><Bath size={20} /> {property?.bathrooms}</div>}
-          {property.parking_spaces && <div className='flexCenter gap-x-2 border-slate-900/50 pr-2 font-[400] text-sm'><CarFront size={20} /> {property?.parking_spaces}</div>}              
-          {/* {property.area && <div className='flexCenter gap-x-2 border-slate-900/50 pr-2 font-[400] text-sm'><Ruler size={20} /> {property?.area} </div>} */}
+          {property.area_size && <div className='flexCenter gap-x-2 border-slate-900/50 pr-2 font-[400] text-sm'><Ruler size={20} /> {property?.area_size}</div>}
+          
         </div> 
 
         <p className="pt-2 mb-2 line-clamp-1">{property.about}</p>
-        <p className="pt-1 mb-2 line-clamp-1">{property.location}</p>
-        {/* <div className="bold-18 mb-4">{property.price_title}</div> */}
+        <p className="pt-1 mb-2 line-clamp-1 flex items-center"> <MapPin size={20} /> &nbsp; {property.location}</p>
+        
         <div 
           className='btn-secondary rounded-xl shadow-sm mt-5 text-center hover:bg-indigo-800 cursor-pointer'>View Details</div>
-          {/* <div 
-          onClick={handleOnClick}
-          className='btn-secondary rounded-xl shadow-sm mt-5 text-center hover:bg-indigo-800 cursor-pointer'>Contact Us</div> */}
-        {/* <EnquireModalBox /> */}
     </div>
   );
 };
