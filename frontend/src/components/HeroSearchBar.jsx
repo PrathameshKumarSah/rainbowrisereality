c03086b330b7c7b60a1266283d903ceca6908e3f
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-29 15:58:13 +0530

    Update HeroSearchBar.jsx

--- frontend/src/components/HeroSearchBar.jsx
+++ frontend/src/components/HeroSearchBar.jsx
@@ -62,7 +62,7 @@ const HeroSearchBar = () => {
           <button
             type="button"
             onClick={handleClear}
-            className="absolute right-40 text-gray-500 hover:text-gray-700"
+            className="absolute  right-12 md:right-40  text-gray-500 hover:text-gray-700"
           >
             <X size={18} />
           </button>
