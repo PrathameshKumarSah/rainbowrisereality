653907379ffb28764fe891c9082adb962c378dbc
Sahil Kumar <123539691+sandy152001@users.noreply.github.com>
2025-01-29 10:19:16 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -1,83 +1,80 @@
-import React from "react";
+import React from 'react'
+import Home2 from '../assets/home2.jpeg'
+import Home4 from '../assets/home4.jpeg'
+import Home5 from '../assets/home5.jpeg'
+import Home3 from '../assets/home3.jpeg'
 
 const Blogs = () => {
-  return (
-    <>
-      <section className="max-padd-container bg-white pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px]">
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-4">
-              <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-                <span className="mb-2 block text-xl font-bold text-black">
-                  Our Blogs
-                </span>
-                <h2 className="mb-4 text-3xl font-bold text-secondary  sm:text-4xl md:text-[40px]">
-                  Our Recent News
-                </h2>
-                <p className="text-base text-body-color dark:text-dark-6">
-                  There are many variations of passages of Lorem Ipsum available
-                  but the majority have suffered alteration in some form.
-                </p>
-              </div>
-            </div>
-          </div>
 
-          <div className="-mx-4 flex flex-wrap">
-            <BlogCard
-              date="Dec 22, 2023"
-              CardTitle="Meet AutoManage, the best AI management tools"
-              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              image="https://i.ibb.co/Cnwd4q6/image-01.jpg"
-            />
-            <BlogCard
-              date="Dec 22, 2023"
-              CardTitle="Meet AutoManage, the best AI management tools"
-              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              image="https://i.ibb.co/Y23YC07/image-02.jpg"
-            />
-            <BlogCard
-              date="Dec 22, 2023"
-              CardTitle="Meet AutoManage, the best AI management tools"
-              CardDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              image="https://i.ibb.co/7jdcnwn/image-03.jpg"
-            />
-          </div>
-        </div>
-      </section>
-    </>
+  return (
+    
+    <div className="p-6 max-w-7xl mx-auto">
+    <h1 className="text-3xl md:text-5xl font-bold text-gray-900">
+      Discover Your Perfect <span className="text-amber-600">Property Match</span>
+    </h1>
+    <p className="mt-2 text-gray-600 max-w-2xl">
+      Discover Your Perfect Property Match with our expert team, dedicated to finding the ideal home or investment on it.
+    </p>
+    <div className="bg-white  py-6 sm:py-8 lg:py-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+      {/* image - start */}
+      <a
+        href="#"
+        className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+      >
+        <img
+          src={Home2}
+          loading="lazy"
+          alt="Photo by Minh Pham"
+          className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+      </a>
+      {/* image - end */}
+      {/* image - start */}
+      <a
+        href="#"
+        className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+      >
+        <img
+          src={Home4}
+          loading="lazy"
+          alt="Photo by Magicle"
+          className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+      </a>
+      {/* image - end */}
+      {/* image - start */}
+      <a
+        href="#"
+        className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+      >
+        <img
+          src={Home5}
+          loading="lazy"
+          alt="Photo by Martin Sanchez"
+          className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+      </a>
+      {/* image - end */}
+      {/* image - start */}
+      <a
+        href="#"
+        className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+      >
+        <img
+          src={Home3}
+          loading="lazy"
+          alt="Photo by Lorenzo Herrera"
+          className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+      </a>
+      {/* image - end */}
+    </div>
+  </div>
+</div>
+  </div>
   );
 };
 
 export default Blogs;
-
-const BlogCard = ({ image, date, CardTitle, CardDescription }) => {
-  return (
-    <>
-      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-        <div className="mb-10 w-full">
-          <div className="mb-8 overflow-hidden rounded">
-            <img src={image} alt="" className="w-full" />
-          </div>
-          <div>
-            {date && (
-              <span className="mb-5 inline-block rounded bg-secondary px-4 py-1 text-center text-base font-semibold leading-loose text-white">
-                {date}
-              </span>
-            )}
-            <h3>
-              <a
-                href="/#"
-                className="mb-4 inline-block text-xl font-bold text-black hover:text-secondary  sm:text-2xl lg:text-xl xl:text-2xl"
-              >
-                {CardTitle}
-              </a>
-            </h3>
-            <p className="text-base text-body-color dark:text-dark-6">
-              {CardDescription}
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
