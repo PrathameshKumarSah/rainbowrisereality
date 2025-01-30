69500d23f503846c8c2ef142d6195bb4c835311c
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-01-30 20:28:47 +0530

    Update Dashboard.jsx

--- frontend/src/admin-components/Dashboard.jsx
+++ frontend/src/admin-components/Dashboard.jsx
@@ -3,10 +3,22 @@ import LatestProperty from './LatestProperty'
 import { apiStore } from '../store/apiHandler'
 
 const Dashboard = () => {
-  const {totalProperties, totalenq} = apiStore();
+  const {totalProperties, totalProjects, projects, totalenq} = apiStore();
   return (
     <div>
       <div className='flex flex-col gap-4 md:flex-row m-4'>
+        <button className="
+          px-4 py-4 flex items-center rounded-xl text-white font-medium 
+      bg-gradient-to-br from-purple-500 to-pink-500 
+      hover:from-pink-500 hover:to-purple-500 
+      transition-colors duration-300 ease-in-out 
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 
+        ">
+          <div className='w-[75%] text-2xl font-bold text-gray-100'>Total Projects</div>
+      
+          <div className='w-[25%] text-4xl font-bold'>{totalProjects}</div>
+        </button>
+
         <button className="
           px-4 py-4 flex items-center rounded-xl text-white font-medium 
       bg-gradient-to-br from-purple-500 to-pink-500 
@@ -18,18 +30,6 @@ const Dashboard = () => {
       
           <div className='w-[25%] text-4xl font-bold'>{totalProperties}</div>
         </button>
-
-{/*         <button className="
-        px-4 py-4 flex items-center rounded-xl text-white font-medium 
-        bg-gradient-to-br from-purple-500 to-pink-500 
-        hover:from-pink-500 hover:to-purple-500 
-        transition-colors duration-300 ease-in-out 
-        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500
-        ">
-          <div className='w-[75%] text-2xl font-bold text-gray-100'>Total Enquires</div>
-      
-          <div className='w-[25%] text-4xl font-bold mx-4'>{totalenq}</div>
-        </button> */}
       </div>
 
       {/* Latest Property Section */}
