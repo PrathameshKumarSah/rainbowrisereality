ce625ad1d3320518aa489585de098ddbb7ee3bb4
Prathamesh Kumar Sah <73576464+PrathameshKumarSah@users.noreply.github.com>
2025-02-03 00:02:44 +0530

    Update RelatedProj.jsx

--- frontend/src/components/RelatedProj.jsx
+++ frontend/src/components/RelatedProj.jsx
@@ -20,7 +20,7 @@ const RelatedProj = ({data, type='project'}) => {
         <div className=" mx-auto p-1 md:p-4 ">
             <div className="flex flex-col bg-[#F8FAFC] shadow-lg rounded-lg overflow-hidden">
                 {/* Left Section */}
-                <div class="h-28 lg:h-64 flex flex-col">
+                <div class="h-28 md:h-52 flex flex-col">
                     <img src={data?.imgs && BASE_URL + data?.imgs.split(',')[0]} alt={data?.imgs && data?.imgs.split(',')[0]}
                     class="h-full w-full object-cover"
                     />
@@ -34,31 +34,31 @@ const RelatedProj = ({data, type='project'}) => {
                                 {data.status}
                             </span>
                         </div>
-                        <h3 className="text-sm md:text-3xl font-bold mt-2 line-clamp-1">{data.title}</h3>
+                        <h3 className="text-sm md:text-xl font-bold mt-2 line-clamp-1">{data.title}</h3>
 
                         <div className="flex flex-col justify-center">
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
                                 {/* Location */}
                                 <div className="flex items-center p-1 md:p-2">
                                     <MapPin className="text-blue-500 mr-1 w-4" />                                    
-                                    <p className="text-xs md:text-sm">{data.location}</p>
+                                    <p className="text-xs lg:text-sm">{data.location}</p>
                                 </div>
                                 
                                 <div className="flex items-center p-1 md:p-2">
-                                    <Building className="text-green-500 mr-1 w-4" />
-                                    <p className="text-xs md:text-sm">{data.category}</p>
+                                    <Building className="text-green-500 mr-1 w-5" />
+                                    <p className="text-xs lg:text-sm">{data.category}</p>
                                 </div>
 
                                 {/* BHK */}
                                 <div className="flex items-center p-1 md:p-2">
                                     <Bed className="text-purple-500 mr-1 w-4" />
-                                    <p className="text-xs md:text-sm">{data.rooms}</p>
+                                    <p className="text-xs lg:text-sm">{data.rooms}</p>
                                 </div>
 
                                 {/* Area Size */}
                                 <div className="flex items-center p-1 md:p-2">
                                     <Ruler className="text-red-500 mr-1 w-4" />
-                                    <p className="text-xs md:text-sm">{data.area_size}</p>
+                                    <p className="text-xs lg:text-sm">{data.area_size}</p>
                                 </div>
                             </div>
                         </div>
